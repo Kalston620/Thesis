a5a292e2914e7b375a7a8096d114fb93b25c7521
Yunhe Wang <yunwa689@student.liu.se>
2024-02-07 16:02:41 +0100

    Change the source to RailML, now with the program that parser the .xml file and plot the structure (This part not done)

--- Station.py
+++
@@ -1,9 +0,0 @@
-class Station:
-    def __init__(self,name,code):
-        self.name=name
-        self.code=code
-
-    def track(self,location,type):
-        self.location=location
-        self.type=type
-        
