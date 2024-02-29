58655022377d322791d4389308d6478808bb8735
Yunhe Wang <yunwa689@student.liu.se>
2024-02-29 11:48:56 +0100

    Typo fix

--- TimeTable_parser.py
+++ TimeTable_parser.py
@@ -12,7 +12,7 @@ def parser(file_path):
                 'start': line_element.get('start'),
                 'end': line_element.get('end'),
                 'route': line_element.get('route'),
-                'timeFronm': line_element.get('timeFrom'),
+                'timeFrom': line_element.get('timeFrom'),
                 'timeTo': line_element.get('timeTo'),
                 'freq': line_element.get('freq'),
                 'stop': line_element.get('stop'),
@@ -23,11 +23,11 @@ def parser(file_path):
     lineTraffics = sorted(lineTraffics, key=lambda x: int(x['priority']))
     return lineTraffics
 
-
+'''
 # Example test
 file_path = 'Katrineholm_TimeTable.xml'
 lineTraffics = parser(file_path)
 
 for line in lineTraffics:
     print( print(f"Line ID: {line['id']}, Line Name: {line['name']}, Start: {line['start']}, End: {line['end']}, Route: {line['route']}, Time From: {line['timeFronm']}, Time To: {line['timeTo']}, Frequency: {line['freq']}, Stop: {line['stop']}, Priority: {line['priority']}"))
-
+'''
