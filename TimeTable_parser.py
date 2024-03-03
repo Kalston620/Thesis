510197ddd4df88868d9cf8e2220d9a880498fea6
Yunhe Wang <yunwa689@student.liu.se>
2024-03-03 15:53:36 +0100

    Change stop into boolean

--- TimeTable_parser.py
+++ TimeTable_parser.py
@@ -7,6 +7,10 @@ def parser(file_path):
     lineTraffics = []
 
     for line_element in root.findall('line'):
+        # Make stop boolean
+        stop_value = line_element.get('stop')
+        stop_boolean = stop_value.lower() == 'true' 
+
         line = {'id': line_element.get('id'),
                 'name': line_element.get('name'),
                 'start': line_element.get('start'),
@@ -15,19 +19,19 @@ def parser(file_path):
                 'timeFrom': line_element.get('timeFrom'),
                 'timeTo': line_element.get('timeTo'),
                 'freq': line_element.get('freq'),
-                'stop': line_element.get('stop'),
+                'stop': stop_boolean,
                 'priority': line_element.get('priority')}
         
         lineTraffics.append(line)
+
     # Rearrange the order according to priority
     lineTraffics = sorted(lineTraffics, key=lambda x: int(x['priority']))
     return lineTraffics
 
-'''
+
 # Example test
 file_path = 'Katrineholm_TimeTable.xml'
 lineTraffics = parser(file_path)
 
 for line in lineTraffics:
-    print( print(f"Line ID: {line['id']}, Line Name: {line['name']}, Start: {line['start']}, End: {line['end']}, Route: {line['route']}, Time From: {line['timeFronm']}, Time To: {line['timeTo']}, Frequency: {line['freq']}, Stop: {line['stop']}, Priority: {line['priority']}"))
-'''
+    print(f"Line ID: {line['id']}, Line Name: {line['name']}, Start: {line['start']}, End: {line['end']}, Route: {line['route']}, Time From: {line['timeFrom']}, Time To: {line['timeTo']}, Frequency: {line['freq']}, Stop: {line['stop']}, Priority: {line['priority']}")
