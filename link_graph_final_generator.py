6a8c5c8e0f9bd102495047fef78666abbbce61be
Yunhe Wang <yunwa689@student.liu.se>
2024-04-09 15:15:07 +0200

    Comment change

--- link_graph_final_generator.py
+++ link_graph_final_generator.py
@@ -3,10 +3,10 @@ def generator(areas):
     
     for i, area in enumerate(areas):
         link_graph[i] = []
-        # 查找与当前区域相邻的其他区域
+        # Find other nabiour areas
         for j, other_area in enumerate(areas):
             if i != j:
-                # 如果两个区域相邻，则将相邻区域的索引添加到列表中
+                # Add to graph if found
                 if area[1] == other_area[0] or area[0] == other_area[1]:
                     link_graph[i].append(j)
     
